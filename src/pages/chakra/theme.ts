838d24aecc9a8a3321c65dd5d5e38f66083cddd0
Abeer Ali Khan <aalkhan@students.uit.edu>
2023-03-03 12:27:28 +0500

    setup chakra and create chakra custom theme

---
+++ src/pages/chakra/theme.ts
@@ -0,0 +1,26 @@
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#FF3c00", // Usage --> brand.100
+    },
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+  styles: {
+    // Global style will applied immediately
+    global: () => ({
+      body: {
+        bg: "gray.200", // Making the body color of entire app gray
+      },
+    }),
+  },
+  components: {
+    // Button
+  },
+});

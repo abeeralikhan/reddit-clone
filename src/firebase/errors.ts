01ed35a59f746074eeb943dd9b40219ed0d16adc
Abeer Ali Khan <aalkhan@students.uit.edu>
2023-03-06 04:44:22 +0500

    update firebase errors

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -1,4 +1,5 @@
 export const FIREBASE_ERRORS = {
   "Firebase: Error (auth/email-already-in-use).":
     "A user with that email already exists!",
+  "Firebase: Error (auth/popup-closed-by-user).": "",
 };

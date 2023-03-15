cbf02b816e22b6b1cfc71860c764c4ca0b184edc
Abeer Ali Khan <aalkhan@students.uit.edu>
2023-03-15 19:21:03 +0500

    implement joining and leaving communities logic using firebase batch writes

--- src/hooks/useCommunityData.ts
+++ src/hooks/useCommunityData.ts
@@ -1,7 +1,14 @@
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  increment,
+  writeBatch,
+} from "firebase/firestore";
 import { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { authModalState } from "../atoms/authModalAtom";
 import {
   Community,
   CommunitySnippet,
@@ -10,6 +17,7 @@ import {
 import { auth, firestore } from "../firebase/clientApp";
 
 const useCommunityData = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
   const [communityStateValue, setCommunityStateValue] =
     useRecoilState(CommunityState);
   const [user] = useAuthState(auth);
@@ -21,8 +29,11 @@ const useCommunityData = () => {
     isJoined: boolean
   ) => {
     // is the user signed in?
-    // if not --> Open Auth Model
-    // return
+    if (!user) {
+      // open the modal
+      setAuthModalState({ open: true, view: "login" });
+      return;
+    }
 
     if (isJoined) {
       leaveCommunity(communityData.id);
@@ -46,8 +57,9 @@ const useCommunityData = () => {
         ...prev,
         mySnippets: snippets as CommunitySnippet[],
       }));
-    } catch (error) {
+    } catch (error: any) {
       console.log("getMySnippets error", error);
+      setError(error.message);
     }
     setLoading(false);
   };
@@ -60,8 +72,81 @@ const useCommunityData = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
 
-  const joinCommunity = (communityData: Community) => {};
-  const leaveCommunity = (communityId: string) => {};
+  const joinCommunity = async (communityData: Community) => {
+    if (error) setError("");
+    setLoading(true);
+    try {
+      const batch = writeBatch(firestore);
+
+      // create a new community snippet
+      const newSnippet: CommunitySnippet = {
+        communityId: communityData.id,
+        imageURL: communityData.imageURL || "",
+      };
+
+      // insert the newSnippet into the communitySnippets collection of the user
+      const snippetsRef = doc(
+        firestore,
+        `users/${user?.uid}/communitySnippets`,
+        communityData.id
+      );
+      batch.set(snippetsRef, newSnippet);
+
+      // update the numberOfMembers inside a communtiy (+1)
+      const communityRef = doc(firestore, "communites", communityData.id);
+      batch.update(communityRef, {
+        numberOfMembers: increment(1),
+      });
+
+      await batch.commit();
+
+      // update recoil state --> communityState.mySnippets
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: [...prev.mySnippets, newSnippet],
+      }));
+    } catch (error: any) {
+      console.log("joinCommunity error", error);
+      setError(error.message);
+    }
+    setLoading(false);
+  };
+
+  const leaveCommunity = async (communityId: string) => {
+    if (error) setError("");
+    setLoading(true);
+    try {
+      const batch = writeBatch(firestore);
+
+      // delete the community snippet
+      const snippetsRef = doc(
+        firestore,
+        `users/${user?.uid}/communitySnippets`,
+        communityId
+      );
+      batch.delete(snippetsRef);
+
+      // update the numberOfMembers inside a communtiy (-1)
+      const communityRef = doc(firestore, "communites", communityId);
+      batch.update(communityRef, {
+        numberOfMembers: increment(-1),
+      });
+
+      await batch.commit();
+
+      // update recoil state --> communityState.mySnippets
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: prev.mySnippets.filter(
+          (snippet) => snippet.communityId !== communityId
+        ),
+      }));
+    } catch (error: any) {
+      console.log("leaveCommunity error", error);
+      setError(error.message);
+    }
+    setLoading(false);
+  };
 
   return {
     communityStateValue,
